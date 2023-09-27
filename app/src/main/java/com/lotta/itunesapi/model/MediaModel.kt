ab558dfa531270d93a3edc92612d7435e3accaa2
lottalai <lotta.lai@fyda.io>
2023-09-27 15:21:27 +0800

    1) add dependency gson 2) set media model field match with API

--- app/src/main/java/com/lotta/itunesapi/model/MediaModel.kt
+++ app/src/main/java/com/lotta/itunesapi/model/MediaModel.kt
@@ -1,10 +1,38 @@
 package com.lotta.itunesapi.model
 
 import androidx.room.Entity
-import java.util.*
 
-@Entity(tableName = "favorites", primaryKeys = ["id"])
+@Entity(tableName = "favorites", primaryKeys = ["artistId", "collectionId", "trackId"])
 data class MediaModel(
-    val id: Int,
-    val releaseDate: Date
+    val artistId: Int,
+    val collectionId: Int,
+    val trackId: Int,
+    val wrapperType: String,
+    val kind: String,
+    val artistName: String,
+    val collectionName: String,
+    val trackName: String,
+    val collectionCensoredName: String,
+    val trackCensoredName: String,
+    val artistViewUrl: String,
+    val collectionViewUrl: String,
+    val trackViewUrl: String,
+    val previewUrl: String,
+    val artworkUrl30: String,
+    val artworkUrl60: String,
+    val artworkUrl100: String,
+    val collectionPrice: Double,
+    val trackPrice: Double,
+    val releaseDate: String,
+    val collectionExplicitness: String,
+    val trackExplicitness: String,
+    val discCount: Int,
+    val discNumber: Int,
+    val trackCount: Int,
+    val trackNumber: Int,
+    val trackTimeMillis: Int,
+    val country: String,
+    val currency: String,
+    val primaryGenreName: String,
+    val isStreamable: Boolean
 )
