3081fc7473e6dc7f3f67b39ef2eadcfe0cfb4d3d
LottaVis_lai <program.lottavis.lai@gmail.com>
2023-10-11 22:28:36 +0800

    1) Splash image 2) App icon

--- app/src/main/java/com/lotta/itunesapi/MainActivity.kt
+++ app/src/main/java/com/lotta/itunesapi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lotta.itunesapi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -20,8 +21,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         mActivity = this
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initNavController()
