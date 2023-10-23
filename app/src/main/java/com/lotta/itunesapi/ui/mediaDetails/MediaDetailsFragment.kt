0668cdc386c9f44827b877283bf43945551cf9f5
LottaVis_lai <program.lottavis.lai@gmail.com>
2023-10-23 21:57:48 +0800

    1) exoplayer buffer

--- app/src/main/java/com/lotta/itunesapi/ui/mediaDetails/MediaDetailsFragment.kt
+++ app/src/main/java/com/lotta/itunesapi/ui/mediaDetails/MediaDetailsFragment.kt
@@ -6,18 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.DefaultLoadControl
+import androidx.media3.exoplayer.ExoPlayer
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.ui.PlayerControlView
 import com.lotta.itunesapi.R
 import com.lotta.itunesapi.databinding.FragmentMediaDetailsBinding
 import com.lotta.itunesapi.model.Track
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
-class MediaDetailsFragment : Fragment() {
+class MediaDetailsFragment : Fragment(){
     private var _binding: FragmentMediaDetailsBinding? = null
     private val binding get() = _binding!!
 
@@ -25,7 +28,7 @@ class MediaDetailsFragment : Fragment() {
     private var track: Track? = null
 
     private val mediaDetailsViewModel: MediaDetailsViewModel by viewModels()
-    private lateinit var exoPlayer: SimpleExoPlayer
+    private lateinit var exoPlayer: ExoPlayer
     private var mediaItem: MediaItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,11 +45,25 @@ class MediaDetailsFragment : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    @androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
+    override fun  onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        exoPlayer = SimpleExoPlayer.Builder(requireContext()).build()
-        binding.playerView.apply { player = exoPlayer }
+        val loadControl = DefaultLoadControl.Builder()
+            .setBufferDurationsMs(
+                // Set the desired buffer size
+                5000, // The minimum buffer size in milliseconds
+                10000, // The maximum buffer size in milliseconds
+                5000, // The buffer for playback in milliseconds
+                5000 // The buffer for reBuffering in milliseconds
+            )
+            .build()
+        exoPlayer = ExoPlayer.Builder(requireContext())
+//            .setLoadControl(loadControl)
+            .build()
+        binding.playerView.apply {
+            player = exoPlayer
+        }
 
         initView()
         initObserve()
@@ -55,9 +72,7 @@ class MediaDetailsFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding.playerView.player = null
-        mediaItem = null
-        exoPlayer.release()
+        releasePlayer()
     }
 
     private fun initViewModel() {
@@ -66,6 +81,9 @@ class MediaDetailsFragment : Fragment() {
 
     private fun initView() {
         track?.let { it ->
+            binding.songNameTextView.text = it.trackName
+            binding.artistTextView.text = it.artistName
+
             if (it.kind.equals("song")) {
                 Glide.with(binding.root).load(it.artworkUrl100).into(binding.thumbnailImageView)
                 binding.thumbnailImageView.visibility = View.VISIBLE
@@ -74,17 +92,32 @@ class MediaDetailsFragment : Fragment() {
             mediaItem?.let { mediaItem -> exoPlayer.setMediaItem(mediaItem) }
 
             exoPlayer.prepare()
-            exoPlayer.play()
-
-            binding.songNameTextView.text = it.trackName
-            binding.artistTextView.text = it.artistName
+            exoPlayer.addListener(object : Player.Listener {
+                override fun onPlaybackStateChanged(playbackState: Int) {
+                    super.onPlaybackStateChanged(playbackState)
+                    when(playbackState){
+                        Player.STATE_IDLE -> {
+                        }
+                        Player.STATE_BUFFERING -> {
+                        }
+                        Player.STATE_ENDED -> {
+                        }
+                        Player.STATE_READY -> {
+                        }
+                    }
+                }
+                override fun onPlayerError(error: PlaybackException) {
+                    super.onPlayerError(error)
+                    releasePlayer()
+                }
+            })
         }
 
-        val controlView =
-            binding.playerView.findViewById<PlayerControlView>(com.google.android.exoplayer2.ui.R.id.exo_controller)
-        controlView.showTimeoutMs = -1
-        controlView.setShowMultiWindowTimeBar(true)
-        controlView.show()
+//        val controlView =
+//            binding.playerView.findViewById<PlayerControlView>(com.google.android.exoplayer2.ui.R.id.exo_controller)
+//        controlView.showTimeoutMs = -1
+//        controlView.setShowMultiWindowTimeBar(true)
+//        controlView.show()
     }
 
     private fun initObserve() {
@@ -107,5 +140,11 @@ class MediaDetailsFragment : Fragment() {
                 mediaDetailsViewModel.setBookmark(track)
             }
         }
+    }
+
+    private fun releasePlayer() {
+        exoPlayer.release()
+        binding.playerView.player = null
+        mediaItem = null
     }
 }
